ea1bcc283eb9878da27fd3744ca7fe5f2ea92bbd
fuga-w <f-watanabe@kk-tsc.com>
2022-12-08 20:35:28 +0900

    プロジェクト ファイルを追加します。

---
+++ BookSearchApp/BookSearcher.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BookSearchApp
+{
+    public class BookSearcher
+    {
+        public BookSearcher() { }
+
+        /// <summary>
+        /// todo: 実装中
+        /// </summary>
+        /// <returns></returns>
+        public List<SearchResult> Search()
+        {
+            
+            var tmpData = new List<SearchResult>();
+            var tnp = new SearchResult("test_name", "test_address", "貸し出し可");
+            tmpData.Add(tnp);
+
+            return tmpData;
+        }
+    }
+}
---
+++ BookSearchApp/SearchResult.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BookSearchApp
+{
+    public class SearchResult
+    {
+        public string libraryName { get; }
+        public string libraryAddress { get; }
+        public string bookState { get; }
+
+        public SearchResult(string libraryName, string libraryAddress, string bookState)
+        {
+            this.libraryName = libraryName;
+            this.libraryAddress = libraryAddress;
+            this.bookState = bookState;
+        }
+    }
+}
